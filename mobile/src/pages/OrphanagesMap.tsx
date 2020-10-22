86585b32803c58aca17aa9d33a597c556d041c7c
Camille Ayume de Melo Kamimura <camille.kamimura@unisoma.com>
2020-10-22 20:16:57 -0300

    Aula 5 - a milha extra

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react'
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import mapMarker from '../images/map-marker.png';
 
+import api from '../services/api';
+
+interface Orphanage {
+    id: number;
+    name: string;
+    latitude: number;
+    longitude: number;
+}
+
 export default function OrphanagesMap() {
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
     const navigation = useNavigation();
 
-    function handleNavigateToOphanageDetails() {
-        navigation.navigate('OrphanageDetails')
+    useFocusEffect(
+        useCallback(() => {
+            api.get('orphanages').then(response => {
+                setOrphanages(response.data);
+            });
+        }, [])
+    );
+
+    function handleNavigateToOphanageDetails(id: number) {
+        navigation.navigate('OrphanageDetails', { id })
+    }
+
+    function handleNavigateToOrphanage() {
+        navigation.navigate('SelectMapPosition')
     }
 
     return (
@@ -21,35 +44,38 @@ export default function OrphanagesMap() {
                 initialRegion={{
                     latitude: -23.9437262,
                     longitude: -46.2985616,
-                    latitudeDelta: 0.008,
-                    longitudeDelta: 0.008,
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005,
                 }}
             >
-                <Marker
-                    icon={mapMarker}
-                    calloutAnchor={{
-                        x: 2.7,
-                        y: 0.8,
-                    }}
-                    coordinate={{
-                        latitude: -23.9437262,
-                        longitude: -46.2985616,
-                    }}
-                >
-                    <Callout tooltip onPress={handleNavigateToOphanageDetails}>
-                        <View style={styles.calloutContainer}>
-                            <Text style={styles.calloutText}>Lar das meninas</Text>
-                        </View>
-                    </Callout>
-                </Marker>
+                {orphanages.map(orphanage => (
+                    <Marker
+                        key={orphanage.id}
+                        icon={mapMarker}
+                        calloutAnchor={{
+                            x: 2.7,
+                            y: 0.8,
+                        }}
+                        coordinate={{
+                            latitude: orphanage.latitude,
+                            longitude: orphanage.longitude,
+                        }}
+                    >
+                        <Callout tooltip onPress={() => handleNavigateToOphanageDetails(orphanage.id)}>
+                            <View style={styles.calloutContainer}>
+                                <Text style={styles.calloutText}>{orphanage.name}</Text>
+                            </View>
+                        </Callout>
+                    </Marker>
+                ))}
             </MapView>
 
             <View style={styles.footer}>
-                <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+                <Text style={styles.footerText}>{orphanages.length} orfanatos encontrados</Text>
 
-                <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
+                <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToOrphanage}>
                     <Feather name="plus" size={20} color="#fff" />
-                </TouchableOpacity>
+                </RectButton>
             </View>
         </View>
     );
